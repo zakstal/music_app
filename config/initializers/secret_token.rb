1c64280b55c637ce264ee9ddfd61e4feace5c5f3
Zak <zak_stallings@zaks-mbp.home>
2014-10-01 22:30:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicApp::Application.config.secret_key_base = 'f15795b025e52cfd5fcb564884e9449b15a258530aced91c552d68b60c5f14c1cda1baac1ca8672340cbdbc0d2689ddf4f6184b5db70107cd60168b9459c39fc'
